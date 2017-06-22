986c826e208ee63ec1dec5ff89004acc4e08a895
Elviss Strazdins <elviss@elviss.lv>
2017-06-23 00:47:45 +0300

    Fix Resource copy script Change order of defines

--- ouzel/core/CompileConfig.h
+++ ouzel/core/CompileConfig.h
@@ -6,10 +6,10 @@
 #if defined(_WIN32)
     #include <sdkddkver.h>
     #define OUZEL_PLATFORM_WINDOWS 1
+    #define OUZEL_SUPPORTS_DIRECT3D 1
     #if _WIN32_WINNT_WIN10
         #define OUZEL_SUPPORTS_DIRECT3D12 1
     #endif
-    #define OUZEL_SUPPORTS_DIRECT3D 1
     #define OUZEL_SUPPORTS_DIRECT3D11 1
     #define OUZEL_SUPPORTS_OPENGL 1
     #define OUZEL_OPENGL_INTERFACE_WGL 1
